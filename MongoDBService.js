b449d1473369c16ad9e8fc50630225ee7999cad4
RazielSurilla <razielsurilla@gmail.com>
2025-03-15 22:55:33 -0700

    Added documentation to MongoDBService class.

--- MongoDBService.js
+++ MongoDBService.js
@@ -1,13 +1,34 @@
+/**
+ * @file MongoDBService.js
+ * @description This file contains the MongoDBService class which is used to connect to the MongoDB database.
+ * @version 1.0.0
+ * @since 16/03/2025
+ */
+
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
+/**
+ * @class MongoDBService
+ * @description This class is used to connect to the MongoDB database.
+ */
 class MongoDBService {
+    
+    /**
+     * @constructor
+     * @description Initialize the MongoDBS connection URI and sets up the database name.
+     */
     constructor() {
         this.uri = this.uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}?retryWrites=true`;
         this.client = null;
         this.database = process.env.MONGODB_DATABASE;
     }
 
+    /**
+     * Connects to the MongoDB database.
+     * @returns {Promise<MongoClient>} The MongoDB client instance.
+     * @throws {Error} If connection to the database fails.
+     */
     async connect() {
         if (this.client) {
             return this.client;
@@ -23,6 +44,11 @@ class MongoDBService {
         }
     }
 
+    /**
+     * Disconnects from the MongoDB database.
+     * @returns {Promise<void>} Resolves when the connection is closed.
+     * @throws {Error} If closing the connection fails or the connection is already closed.
+     */
     async disconnect() {
         if (!this.client) {
             return;
@@ -33,6 +59,12 @@ class MongoDBService {
         console.log('Connection to MongoDB closed');
     }
 
+    /**
+     * Connects to a specific collection in the database.
+     * @param {string} collectionName The name of the collection to connect to.
+     * @returns {Promise<Collection>} The collection instance.
+     * @throws {Error} If connection to the collection fails.   
+     */
     async connectToCollection(collectionName) {
         try {
             const client = await this.connect();
