12501ea87e23f974f73b4513cfd260b4f2854d04
RazielSurilla <razielsurilla@gmail.com>
2025-03-22 15:12:29 -0700

    changed SameSite to None since frontend and backend are hosted on different domains

--- MongoAPIService.js
+++ MongoAPIService.js
@@ -203,7 +203,7 @@ class MongoAPIService {
 
             //Creates a signed token by jwt, and attach it to httpCookie
             const token = jwt.sign({ userId: user._id, email: user.email }, 'your_jwt_secret_key', { expiresIn: '1h' });
-            res.cookie('userCookie', token, {httpOnly: true, secure : true, SameSite: 'Lax'});
+            res.cookie('userCookie', token, {httpOnly: true, secure : true, SameSite: 'None'});
             res.status(200).json({ message: 'Login successful', admin: user.admin, username: user.username});
         } catch (error) {
             res.status(500).json({ message: 'Error logging in: ' + error.message });
